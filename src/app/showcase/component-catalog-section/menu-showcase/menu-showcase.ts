afa2acf86e257d5c4bd3b5a0852ca5931a7897e3
Nurlan Joldibaev <nurlanjoldibaev7@gmail.com>
2026-06-27 05:04:39 +0500

    menu fix

--- src/app/showcase/component-catalog-section/menu-showcase/menu-showcase.ts
+++ src/app/showcase/component-catalog-section/menu-showcase/menu-showcase.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { UiButton } from '../../../components/ui-button/ui-button';
+import { UiIcon } from '../../../components/ui-icon/ui-icon';
 import { UiMenu } from '../../../components/ui-menu/ui-menu';
 import { UiMenuItem } from '../../../components/ui-menu/ui-menu-item/ui-menu-item';
 import { UiMenuTrigger } from '../../../components/ui-menu/ui-menu-trigger/ui-menu-trigger';
 
 @Component({
   selector: 'app-menu-showcase',
-  imports: [UiButton, UiMenu, UiMenuItem, UiMenuTrigger],
+  imports: [UiButton, UiIcon, UiMenu, UiMenuItem, UiMenuTrigger],
   templateUrl: './menu-showcase.html',
 })
 export class MenuShowcase {
